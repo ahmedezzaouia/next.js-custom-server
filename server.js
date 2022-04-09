be902677223b3cac656af47479e83939c021c8de
Ahmed Ez zaouia <ahmez-za@e1r13p9.1337.ma>
2022-04-09 08:11:05 +0000

    change server code

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
-const port = 3000;
-const host = "127.0.0.1";
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
 
-const app = next({ dev });
 const hundle = app.getRequestHandler();
 app
   .prepare()
@@ -25,7 +26,7 @@ app
       return hundle(req, res);
     });
 
-    server.listen(port, () => console.log(`server running at https://${host}:${port}`));
+    server.listen(port, () => console.log(`server running at https://${hostname}:${port}`));
   })
   .catch((err) => {
     console.log(err.stack);
